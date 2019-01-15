979ed3a35ac67d613697aed8db4076820f05e329
litiansuo <2296923560@qq.com>
2019-01-15 17:26:17 +0800

    加入敏感字符替换系统

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 char arr[1000];
 int index = 0;
+int isMingan(char x){
+    int count = 0;
+    for(int i=0;i<index;i++){
+        if(x == arr[i]){
+            count++;
+        }
+    }
+    if(count == 0){
+        return 0;
+    }else{
+        return 1;
+    }
+
+}
+  
 int main()
 {
 
@@ -53,6 +68,25 @@ int main()
         }
         if(code == 4)
         {
+            printf("qingshuruyiduanyingwen\n");
+            char neirong[1000];
+            scanf("%s",neirong);
+            for(int i = 0;1;i++){
+                if(neirong[i] == '\0'){
+                    break;
+                }
+                if(isMingan(arr[i]) == 1){
+                    neirong[i] = '*';
+                }
+                printf("%c",neirong[i]);
+            }
+            printf("\n");
+
+            printf("tihuanwancheng,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+            
         }
         if (code == 5)
         {
